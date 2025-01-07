3e8627b9673560307ae11a22c5febe5e7833ae35
ryaanf <farrukh.ryaan@gmail.com>
2025-01-07 10:22:45 -0500

    update

--- script.js
+++ script.js
@@ -2,11 +2,12 @@ particlesJS.load('particles-js', 'particles.json', function () {
     console.log('Particles.js loaded');
   });
   
+  document.body.classList.add('dark');
+
   const themeToggle = document.getElementById('theme-toggle');
+  themeToggle.textContent = '☀️';
   themeToggle.addEventListener('click', () => {
     document.body.classList.toggle('dark');
     themeToggle.textContent = document.body.classList.contains('dark') ? '☀️' : '🌙';
   });
-  
-  document.body.classList.add('light');
   
