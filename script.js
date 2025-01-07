ea810c3baf5bf901030736de0235092da5562509
ryaanf <farrukh.ryaan@gmail.com>
2025-01-07 13:21:11 -0500

    changes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-particlesJS.load('particles-js', 'particles.json', function () {
+particlesJS.load('particles-js', './particles.json', function () {
     console.log('Particles.js loaded');
   });
   
